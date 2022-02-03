152deff21c9ced41004e7e384a9fe9193cd9f28d
gantoin <gantoin@pm.me>
2022-02-03 08:09:17 +0100

    Read csv for days and months

---
+++ src/main/java/fr/gantoin/quizzukrainiannumber/domain/Day.java
@@ -0,0 +1,15 @@
+package fr.gantoin.quizzukrainiannumber.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Day {
+
+    private String english;
+    private String ukrainian;
+
+}
--- src/main/java/fr/gantoin/quizzukrainiannumber/service/CsvReader.java
+++ src/main/java/fr/gantoin/quizzukrainiannumber/service/CsvReader.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.Getter;
 
+import fr.gantoin.quizzukrainiannumber.domain.Day;
+import fr.gantoin.quizzukrainiannumber.domain.Month;
 import fr.gantoin.quizzukrainiannumber.domain.Number;
 
 @Service
@@ -22,13 +24,52 @@ public class CsvReader {
     @Getter
     private final List<Number> numbers = new ArrayList<>();
 
+    @Getter
+    private final List<Day> days = new ArrayList<>();
+
+    @Getter
+    private final List<Month> months = new ArrayList<>();
+
     public CsvReader() throws URISyntaxException, IOException {
-        InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("static/numbers.csv");
-        CsvReader.readFile(resourceAsStream).forEach(line -> {
+        initNumbers();
+        initDays();
+        initMonths();
+    }
+
+    private void initMonths() throws IOException {
+        InputStream monthStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("static/months.csv");
+        CsvReader.readFile(monthStream).forEach(line -> {
+            String[] strings = line.split(",");
+            Month month = new Month(strings[0], strings[1]);
+            months.add(month);
+        });
+        if (monthStream != null) {
+            monthStream.close();
+        }
+    }
+
+    private void initDays() throws IOException {
+        InputStream dayStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("static/days.csv");
+        CsvReader.readFile(dayStream).forEach(line -> {
+            String[] strings = line.split(",");
+            Day day = new Day(strings[0], strings[1]);
+            days.add(day);
+        });
+        if (dayStream != null) {
+            dayStream.close();
+        }
+    }
+
+    private void initNumbers() throws IOException {
+        InputStream numberStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("static/numbers.csv");
+        CsvReader.readFile(numberStream).forEach(line -> {
             String[] strings = line.split(",");
             Number number = new Number(Integer.parseInt(strings[0]), strings[1]);
             numbers.add(number);
         });
+        if (numberStream != null) {
+            numberStream.close();
+        }
     }
 
     public static List<String> readFile(InputStream resourceAsStream) throws IOException {
@@ -48,5 +89,15 @@ public class CsvReader {
     public Number getRandomNumber() {
         Random random = new Random();
         return numbers.get(random.nextInt(numbers.size()));
+    }
+
+    public Day getRandomDay() {
+        Random random = new Random();
+        return days.get(random.nextInt(days.size()));
+    }
+
+    public Month getRandomMonth() {
+        Random random = new Random();
+        return months.get(random.nextInt(months.size()));
     }
 }
